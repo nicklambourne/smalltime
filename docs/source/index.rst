8649dd79f5a6733dbed005f5bd21dd11abe9ec23
nicklambourne <ndl93@live.com>
2021-10-17 21:43:40 +1100

    .

---
+++ docs/source/index.rst
@@ -0,0 +1,18 @@
+Welcome to smalltime's documentation!
+=====================================
+
+.. automodule:: smalltime.timer
+    :members:
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
--- smalltime/timer.py
+++ smalltime/timer.py
@@ -1,19 +1,28 @@
 import functools
-import sys
 import time
 
-from typing import Callable, Optional, TextIO
+from typing import Callable, Optional
 
 import colors
 import shortuuid
 
 
 class SmallTimer:
+    """
+    A semi-accurate timer with start and stop capabilities.
+
+    :arg name: an optional label for identifying
+    """
     def __init__(self, name: Optional[str] = None):
         self.name = name if name else shortuuid.uuid()[:8]
-        self.stored_time = None
+        self.stored_time = 0
 
     def start(self) -> "SmallTimer":
+        """
+        Starts the timer.
+
+        :return: the started timer object.
+        """
         print(
             colors.color(f"Starting counter ({self.name})", fg="white", bg="red"),
         )
@@ -21,6 +30,11 @@ class SmallTimer:
         return self
 
     def stop(self) -> int:
+        """
+        Stops the timer.
+
+        :return: the number of nanoseconds between starting and stopping the timer
+        """
         if not self.stored_time:
             raise RuntimeError("Timers must be started before they can be stopped.")
         time_delta_ns = time.perf_counter_ns() - self.stored_time
@@ -31,11 +45,16 @@ class SmallTimer:
                 bg="red",
             ),
         )
-        self.stored_time = None
+        self.stored_time = 0
         return time_delta_ns
 
 
-def timed(name: Optional[str] = None):
+def timed(name: Optional[str] = None) -> Callable:
+    """
+    A decorator for timing functions.
+
+    :param name: an optional label for the timer
+    """
     def decorator_timed(func: Callable):
         @functools.wraps(func)
         def wrapper_timed(*args, **kwargs):
